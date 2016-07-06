31edbeb1f696e3e1ed9a3dce411ce2eaf468977d
sevriugin <sevriugin@gmail.com>
2016-07-06 17:54:42 +0200

    fix ssl

--- server.js
+++ server.js
@@ -244,6 +244,15 @@ var SampleApp = function() {
     };
 
 
+    
+    self.redirectSec = function(req, res, next) {
+    	  if (req.headers['x-forwarded-proto'] == 'http') {
+    	      res.redirect('https://' + req.headers.host + req.path);
+    	  } else {
+    	      return next();
+    	  }
+    };
+    
     /**
      *  Initialize the server (express) and create the routes and register
      *  the handlers.
@@ -254,7 +263,7 @@ var SampleApp = function() {
 
         //  Add handlers for the app (from the routes).
         for (var r in self.routes) {
-            self.app.get(r, self.routes[r]);
+            self.app.get(r, self.redirectSec, self.routes[r]);
         }
         
         self.app.get('/api/OAuth2', urlencodedParser, function(req, res) {
